048772aebc589cae7d22da46006f87ad4a11f5a4
DivineHollow <egorich7250@gmail.com>
2025-05-02 00:20:31 +0300

    Commit2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -6,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float movementSpeedY = 10;
     [SerializeField] private SpriteRenderer sprite;
     [SerializeField] private Rigidbody2D body;
+    [SerializeField] private Animator anim;
+    [SerializeField] private bool grounded;
+    private bool canDoubleJump;
 
 
     private void Update()
@@ -18,8 +22,25 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontalInput < 0f)
             sprite.flipX = true;
 
-        if (Input.GetKeyDown(KeyCode.Space))
-            body.linearVelocity = new Vector2(body.linearVelocityX, movementSpeedY);
+        if (Input.GetKeyDown(KeyCode.Space) && (grounded || canDoubleJump))
+        {
+            canDoubleJump = grounded;
+            Jump();
+        }
+
+        anim.SetBool("Run", Mathf.Abs(horizontalInput) > 0.5f);
+        anim.SetBool("Grounded", grounded);
     }
 
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.linearVelocityX, movementSpeedY);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
 }
