174287739959b3a0f8eec46bccebe486842413fc
DivineHollow <egorich7250@gmail.com>
2025-06-15 16:43:06 +0300

    PauseMenu + VolumeChange

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,12 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [Header ("Game Over")]
     [SerializeField] private GameObject gameOverScreen;
     [SerializeField] private AudioClip gameOverSound;
+
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseScreen;
 
     private void Awake()
     {
         gameOverScreen.SetActive(false);
+        pauseScreen.SetActive(false);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame(!pauseScreen.activeInHierarchy);
+        }
     }
 
     #region Game Over Functions
@@ -32,6 +44,26 @@ public class UIManager : MonoBehaviour
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
+    }
+    #endregion
+
+    #region Pause
+    public void PauseGame(bool status)
+    {
+        pauseScreen.SetActive(status);
+
+        if (status)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+    public void SFXVolume()
+    {
+        SoundManager.instance.ChangeSFXVolume(0.2f);
+    }
+    public void MusicVolume()
+    {
+        SoundManager.instance.ChangeMusicVolume(0.2f);
     }
     #endregion
 }
